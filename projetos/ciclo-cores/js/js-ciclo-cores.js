4a53efb811beba18e025e89f6e9d3dfc1aa08b1b
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-07-03 01:30:26 -0300

    ciclo-cores

--- projetos/ciclo-cores/js/js-ciclo-cores.js
+++ projetos/ciclo-cores/js/js-ciclo-cores.js
@@ -1,19 +1,123 @@
+let circle = document.getElementById('circle')
+let checkBox = document.getElementById('check')
+let pushColor = document.getElementById('pushColor')
+let inputColor = document.getElementById('inputColor')
+let inputTime = document.getElementById('inputTime')
+let cleanColor = document.getElementById('cleanColor')
+let aviso = document.getElementById('aviso')
 
+let cores = []
 
+let i = 0
+let time = parseFloat('3') * 1000
 
-
-let i = 1;                  //  set your counter to 1
-let time = parseFloat('0.25') * 1000
-console.log(time)
-
-function myLoop() {         //  create a loop function
-  setTimeout(function() {   //  call a 3s setTimeout when the loop is called
-    console.log('hello');   //  your code here
-    i++;                    //  increment the counter
-    if (i < 10) {           //  if the counter < 10, call the loop function
-      myLoop();             //  ..  again which will trigger another 
-    }                       //  ..  setTimeout()
+function myLoop() {
+  setTimeout(function() {
+      if (checkBox.checked == false) {
+        i = 1000
+    } 
+    circle.style.background = `${cores[i]}`
+    console.log(i);
+    i++
+    if (i < cores.length) {
+      myLoop()
+    } else if (i == cores.length) {
+        i = 0
+        myLoop()
+    }
   }, time)
 }
 
-myLoop();                   //  start the loop
+function check() {
+    if (checkBox.checked == false) {
+        inputColor.readOnly = false
+        inputTime.readOnly = false
+
+        inputColor.style.cursor = 'auto'
+        inputTime.style.cursor = 'auto'
+        pushColor.style.cursor = 'pointer'
+        cleanColor.style.cursor = 'pointer'
+
+        inputColor.style.userSelect = 'auto'
+        inputTime.style.userSelect = 'auto'
+
+        pushColor.setAttribute('class', 'pushColorOFF')
+
+        circle.style.background = '#292b2c'
+        circle.style.boxShadow = '-4px -4px 10px rgba(255, 255, 255, 0.05), 4px 4px 10px rgba(0, 0, 0, 0.3), inset -4px -4px 10px rgba(255, 255, 255, 0.05), inset 4px 4px 10px rgba(0, 0, 0, 0.3)'
+    } else {
+        inputColor.readOnly = true
+        inputTime.readOnly = true
+
+        inputColor.style.cursor = 'default'
+        inputTime.style.cursor = 'default'
+        pushColor.style.cursor = 'default'
+        cleanColor.style.cursor = 'default'
+
+        inputColor.style.userSelect = 'none'
+        inputTime.style.userSelect = 'none'
+
+        pushColor.setAttribute('class', '')
+
+        if (cores.length != '0') {
+            if (inputTime.value != '') {
+                time = parseFloat(inputTime.value) * 1000
+            }
+            console.log(cores)
+            circle.style.boxShadow = '-4px -4px 10px rgba(255, 255, 255, 0.05), 4px 4px 10px rgba(0, 0, 0, 0.3)'
+
+            circle.style.background = `${cores[0]}`
+
+            i = 1
+            myLoop()
+        }
+    }
+}
+
+function isInputNumberAnpPoint(event) {
+    let char = String.fromCharCode(event.which)
+
+    if (!(/^[0-9.]+$/.test(char))) {
+        event.preventDefault()
+    }
+}
+
+function isInputHex(event) {
+    let char = String.fromCharCode(event.which)
+
+    if (!(/^[0-9A-Fa-f]+$/.test(char))) {
+        event.preventDefault()
+    }
+}
+
+function AdicionarColor() {
+    if (checkBox.checked == false) {
+        if (inputColor.value.length == '3' || inputColor.value.length == '6') {
+            aviso.style.display = 'none'
+            cores.push('#' + inputColor.value)
+        } else {
+            console.log(inputColor.value.length);
+            aviso.style.display = 'initial'
+        }
+
+        inputColor.value = ''
+    }
+}
+
+function limparCor() {
+    if (checkBox.checked == false) {
+        cores = []
+    }
+}
+
+function mouseDown() {
+    if (checkBox.checked == false) {
+        cleanColor.style.fontSize = '0.95rem'
+        cleanColor.style.boxShadow = '-4px -4px 10px rgba(255, 255, 255, 0.05), 4px 4px 10px rgba(0, 0, 0, 0.3), inset -4px -4px 10px rgba(255, 255, 255, 0.05), inset 4px 4px 10px rgba(0, 0, 0, 0.3)'
+    }
+}
+
+function mouseUp() {
+    cleanColor.style.fontSize = '1rem'
+    cleanColor.style.boxShadow = '-4px -4px 10px rgba(255, 255, 255, 0.05), 4px 4px 10px rgba(0, 0, 0, 0.3)'
+}
